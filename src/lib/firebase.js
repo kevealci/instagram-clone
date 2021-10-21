8976d43731ec22168d59044b272c9a786e1de159
kevealci <kevealci@espol.edu.ec>
2021-10-21 15:19:36 -0500

    Configuration and generation of users and photos in FireBase

---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// here I want to import the seed file
+// import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyAcJzOoEX_kety8dkSwTI0HHZ2AwpnRZxo',
+  authDomain: 'instagram-clone-7057b.firebaseapp.com',
+  projectId: 'instagram-clone-7057b',
+  storageBucket: 'instagram-clone-7057b.appspot.com',
+  messagingSenderId: '942306765196',
+  appId: '1:942306765196:web:7b25726254a12e81c60ea9'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// here is where I want to call the seed file (only ONCE!)
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
