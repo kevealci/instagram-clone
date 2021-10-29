86e70f0f12d7446ccf27d5f930ea93e8a0deb490
kevealci <kevealci@espol.edu.ec>
2021-10-29 08:59:42 -0500

    Post header finished

---
+++ src/components/Post/index.js
@@ -0,0 +1,24 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import Header from './Header';
+
+export default function Post({ content }) {
+  return (
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+      <Header username={content.username} />
+    </div>
+  );
+}
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string,
+    imageSrc: PropTypes.string,
+    caption: PropTypes.string,
+    docId: PropTypes.string,
+    userLikedPhoto: PropTypes.bool,
+    likes: PropTypes.array,
+    comments: PropTypes.array,
+    dateCreated: PropTypes.number
+  })
+};
